95026366b4d6e117ae0c0f0dfa770eb849373ae6
Chen Sicong <sicong.chen@163.com>
2020-09-15 15:50:30 +0800

    [Chen Sicong]feat: add avatar-image

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -6,12 +6,14 @@ const fetchUserInfo = async (userId) => {
   const basicInfoName = document.getElementById("basic-info-name");
   const basicInfoAge = document.getElementById("basic-info-age");
   const aboutMeContent = document.getElementById("about-me-content");
+  const avatar = document.getElementById("avatar-image");
   const response = await fetch(`http://localhost:8080/users/${userId}`);
   if (isRequestSuccess(response.status)) {
     const data = await response.json();
     basicInfoName.innerText = data.name;
     basicInfoAge.innerText = `${data.age}YO`;
     aboutMeContent.innerText = data.description;
+    avatar.setAttribute("src", data.avatar);
   }
 };
 
